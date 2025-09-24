1b32ab0347f6a83f9f1f9a5acd31770671ce8988
김재연 <jaeyeon_kim@bigin.io>
2025-09-24 18:33:56 +0900

    Add: User-Class-Service

---
+++ user-class-service/src/main/java/com/example/userclassservice/UserClassServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.userclassservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserClassServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserClassServiceApplication.class, args);
+    }
+
+}
---
+++ user-class-service/src/test/java/com/example/userclassservice/UserClassServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userclassservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserClassServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
